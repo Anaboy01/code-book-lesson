2032056e6c61dd76e9f91d1036873d95089f81d8
anaboy01 <aliyuanate016@gmail.com>
2026-06-30 14:48:50 +0100

    we

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,10 @@
+const frontendURL = process.env.CLIENT_URL;
+
 const allowedOrigin = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:3000',
+    frontendURL,
 ];
 
 module.exports = allowedOrigin;
